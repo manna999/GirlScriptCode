fbaf206abc0c152a82aaac2bd6ac660bfa9adc4d
manna999 <60973878+manna999@users.noreply.github.com>
2020-06-18 16:56:46 +0100

    Add files via upload

---
+++ Hw1.cpp
@@ -0,0 +1,24 @@
+# include <iostream>
+using namespace std;
+
+int main()
+{
+	int a,b,c;
+	
+	cout << "Enter three numbers:";
+	cin >> a >> b >> c;
+	
+	if ((a>=b) && (a>=c))
+	cout << "Largest number:" << a;
+	else if ((b>=a) && (b>=c))
+	cout << "Largest number:" << b;
+	else
+	cout << "Largest number:" << c;
+	
+	return 0;
+}
+
+
+
+ 
+
